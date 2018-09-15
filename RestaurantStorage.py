ba9571d42f0f03946e1029994f710d6e8ecc3846
Hayley Bergman <hayberg@mit.edu>
2018-09-15 13:17:52 -0400

    Created RestaurantStorage

---
+++ RestaurantStorage.py
@@ -0,0 +1,24 @@
+class RestaurantStorage:
+
+	def __init__(self):
+		self.categoryMap = {}
+		self.allRestaurants = []
+
+	def addRestaurant(restaurant):
+		allRestaurants.append(restaurant.name)
+		addHelper(restaurant)
+
+	def addHelper(restaurant):
+		if category in categoryMap.keys()
+			currentInCategory = categoryMap[restaurant.category]
+			for i in range(len(currentInCategory)):
+				if restaurant.rating >= currentInCategory[i].rating:
+					currentInCategory.insert(i,restaurant)
+						categoryMap[restaurant.category] = currentInCategory
+							return
+			currentInCategory.append(restaurant)
+			categoryMap[restaurant.category] = currentInCategory
+			return
+		else:
+			categoryMap[restaurant.category] = [restaurant]
+
