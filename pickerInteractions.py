f9d2463510405db875fd54c86fe600b238673615
Emily Zhang <emizhang@mit.edu>
2018-09-15 14:02:29 -0400

    made commands looped

--- pickerInteractions.py
+++ pickerInteractions.py
@@ -6,23 +6,29 @@ r1 = Restaurant("pep", 5, "thai")
 restaurants.addRestaurant(r1)
 
 i = input("Welcome to Personal Restaurant Picker, tell me what you would like to do: ")
+running = True
 
-if i == "where should I eat":
-	print(restaurants.getAnyRestaurant().to_string())
+while running:
 
-elif i == "add a restaurant":
-	name = input("What is the restaurant's name? ")
-	rating = float(input("How would you rate " + name + " out of 5? "))
-	category = input("What type of cuisine does " + name + " serve?")
-	r = Restaurant(name, rating, category)
-	restaurants.addRestaurant(r)
+	if i == "where should I eat":
+		print(restaurants.getAnyRestaurant().to_string())
+
+	elif i == "add a restaurant":
+		name = input("What is the restaurant's name? ")
+		rating = float(input("How would you rate " + name + " out of 5? "))
+		category = input("What type of cuisine does " + name + " serve?")
+		r = Restaurant(name, rating, category)
+		restaurants.addRestaurant(r)
 
 
-elif i == "get restaurant in category":
-	category = input("What type of cuisine do you want?")
-	r = restaurants.getByCategory(category)
-	if r == []:
-		print("You don't have any restaurants in that category...you should get out more")
-	else:
-		for res in r:
-			print(res.to_string())
+	elif i == "get restaurant in category":
+		category = input("What type of cuisine do you want?")
+		r = restaurants.getByCategory(category)
+		if r == []:
+			print("You don't have any restaurants in that category...you should get out more")
+		else:
+			for res in r:
+				print(res.to_string())
+
+	elif i == "quit":
+		running = False
